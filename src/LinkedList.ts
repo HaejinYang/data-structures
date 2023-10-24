d460d00b4ceb6ffb8a238d754e2a6cb05af459ac
haejin <haejin.yang91@gmail.com>
2023-10-24 21:14:30 +0900

    feat: 링크드리스트 구현 및 테스트 완료

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -10,7 +10,7 @@ class LinkedList<T> {
     this.tail.prev = this.head;
   }
 
-  insert(item: T) {
+  insertEnd(item: T): Node<T> {
     const prev = this.tail.prev;
 
     const newNode = new Node<T>();
@@ -19,6 +19,38 @@ class LinkedList<T> {
     newNode.prev = prev;
     prev.next = newNode;
     this.tail.prev = newNode;
+
+    return newNode;
+  }
+
+  insertBegin(item: T): Node<T> {
+    const next = this.head.next;
+    const newNode = new Node<T>();
+    newNode.data = item;
+
+    this.head.next = newNode;
+    newNode.prev = this.head;
+    newNode.next = next;
+    next.prev = newNode;
+
+    return newNode;
+  }
+
+  insertAfterNode(node: Node<T>, item: T): Node<T> {
+    if (!this.find(node.data)) {
+      throw new Error("링크드리스트에 속한 노드가 아님");
+    }
+
+    const next = node.next;
+
+    const newNode = new Node<T>();
+    newNode.data = item;
+    newNode.prev = node;
+    newNode.next = next;
+    node.next = newNode;
+    next.prev = newNode;
+
+    return newNode;
   }
 
   all() {
@@ -32,10 +64,21 @@ class LinkedList<T> {
     return result;
   }
 
+  revers() {
+    let result: T[] = [];
+    let node = this.tail.prev;
+    while (node !== this.head) {
+      result.push(node.data);
+      node = node.prev;
+    }
+
+    return result;
+  }
+
   /**
    *
    * @param item
-   * @return 찾는 아이템의 포지션, 못찾으면 -1
+   * @return 찾으면 Node, 못찾으면 null
    */
   find(item: T): Node<T> | null {
     let node = this.head.next;
@@ -48,6 +91,51 @@ class LinkedList<T> {
     }
 
     return null;
+  }
+
+  has(node: Node<T>): boolean {
+    let find = this.head.next;
+    while (find !== this.tail) {
+      if (find === node) {
+        return true;
+      }
+
+      find = find.next;
+    }
+
+    return false;
+  }
+
+  /**
+   * @param this
+   * @param item
+   * @returns 응답으로 자신을 반환. 리턴으로 성공 실패를 줘야하나 싶었지만, 지운다는 것의 결과로 성공 실패를 받는게 사용자에게 이점이 없다.
+   */
+  remove(item: T): this {
+    const node = this.find(item);
+    if (node) {
+      const prev = node.prev;
+      const next = node.next;
+
+      prev.next = next;
+      next.prev = prev;
+    }
+
+    return this;
+  }
+
+  removeNode(node: Node<T>): this {
+    if (!this.has(node)) {
+      throw new Error("링크드리스트에 속한 노드가 아님");
+    }
+
+    const prev = node.prev;
+    const next = node.next;
+
+    prev.next = next;
+    next.prev = prev;
+
+    return this;
   }
 }
 
--- tests/LinkedList.test.ts
+++ tests/LinkedList.test.ts
@@ -1,27 +1,102 @@
 import { LinkedList } from "../src/LinkedList";
 
-test("if insert nodes, all() return them", () => {
+test("InsertEnd_MultipleItems_ReturnSameOrders", () => {
   const list = new LinkedList<number>();
-  list.insert(10);
-  list.insert(20);
-  list.insert(30);
+  list.insertEnd(10);
+  list.insertEnd(20);
+  list.insertEnd(30);
 
-  expect(list.all()).toEqual([10, 20, 30]);
+  const actual = list.all();
+
+  expect(actual).toEqual([10, 20, 30]);
 });
 
-test("if no nodes, find() retunr -1", () => {
+test("InsertAfterNode_Item_ReturnNext", () => {
   const list = new LinkedList<number>();
+  list.insertEnd(10);
+  list.insertEnd(20);
+  list.insertEnd(30);
 
-  expect(list.find(10)).toEqual(null);
+  const node = list.find(20);
+  list.insertAfterNode(node, 50);
+  const actual = list.all();
+
+  expect(actual).toEqual([10, 20, 50, 30]);
 });
 
-test("if insert nodes, find() retunr position", () => {
+test("InsertBegin_Item_ReturnNodeAtFirst", () => {
   const list = new LinkedList<number>();
-  list.insert(10);
-  list.insert(20);
-  list.insert(30);
+  list.insertEnd(10);
+  list.insertEnd(20);
+  list.insertEnd(30);
 
-  expect(list.find(10).data).toEqual(10);
-  expect(list.find(20).data).toEqual(20);
-  expect(list.find(30).data).toEqual(30);
+  list.insertBegin(100);
+  const actual = list.all();
+
+  expect(actual).toEqual([100, 10, 20, 30]);
+});
+
+test("Find_ItemNotInList_ReturnNull", () => {
+  const list = new LinkedList<number>();
+  const item = 10;
+
+  const actual = list.find(item);
+
+  expect(actual).toEqual(null);
+});
+
+test("Find_ItemInList_ReturnNode", () => {
+  const list = new LinkedList<number>();
+  list.insertEnd(10);
+  const target = list.insertEnd(20);
+  list.insertEnd(30);
+
+  const actual = list.find(20);
+
+  expect(actual).toEqual(target);
+});
+
+test("Has_NodeInList_ReturnNode", () => {
+  const list = new LinkedList<number>();
+  list.insertEnd(10);
+  list.insertEnd(20);
+
+  const target = list.insertEnd(30);
+  const actual = list.has(target);
+
+  expect(actual).toEqual(true);
+});
+
+test("Remove_ItemInList_ReturnWithoutThem", () => {
+  const list = new LinkedList<number>();
+  list.insertEnd(10);
+  list.insertEnd(20);
+  list.insertEnd(30);
+
+  const actual = list.remove(20).all();
+
+  expect(actual).toEqual([10, 30]);
+});
+
+test("Remove_NodeInList_ReturnWithoutIt", () => {
+  const list = new LinkedList<number>();
+  list.insertEnd(10);
+  list.insertEnd(20);
+
+  const target = list.insertEnd(30);
+  list.removeNode(target);
+  const actual = list.has(target);
+
+  expect(actual).toEqual(false);
+});
+
+test("Reverse_Items_ReturnReverseOrder", () => {
+  const list = new LinkedList<number>();
+  list.insertEnd(10);
+  list.insertEnd(20);
+  list.insertEnd(30);
+
+  const actual = list.revers();
+
+  expect(actual).toEqual([30, 20, 10]);
 });
