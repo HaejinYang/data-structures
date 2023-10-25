deb22a426a2a6bec06c57b2a6a6c22c1d922c00f
haejin <haejin.yang91@gmail.com>
2023-10-25 16:53:35 +0900

    chore: 컴파일 옵션에 'isolatedModules: true'를 추가하고 그 과정을 리드미에 반영

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]s$",
+  transform: {
+    "^.+\\.ts?$": ["ts-jest", { isolatedModules: true }],
+  },
 };
