499854b3d65796e6945f1f4dd37e89e3ce43f794
haejin.yang91 <haejin.yang91@gmail.com>
2023-11-12 18:36:26 +0900

    refactor: 힙으로 정돈하는 것을 상향, 하향 둘로 나눔

--- src/Heap.ts
+++ src/Heap.ts
@@ -9,7 +9,7 @@ export class Heap<T> {
 
   push(item: T) {
     this.store.push(item);
-    this.heapify();
+    this.heapifyToUpward();
   }
 
   empty(): boolean {
@@ -30,12 +30,41 @@ export class Heap<T> {
     }
 
     this.store.shift();
+    this.heapifyToDownward();
+  }
 
+  all(): T[] {
+    return this.store;
+  }
+
+  private heapifyToUpward(): void {
     /**
-     * 왜 2번 도나? push와 pop이 반복되면, 
-     * 최소힙인데 루트가 자식 둘보다 크거나, 
-     * 최대힙인데 루트가 자식 둘보다 작은 상황이 발생할 수 있음.
-     * ex) 0, 10, 100, -10, -100 push후 두 번 pop하면 [100, 10, 0]이 되고 만약 한 번만 루프가 돌면 [10, 100, 0]이 됨.
+     *    1. 가장 후미에 아이템 삽입
+     *    2. 아이템의 부모, 부모의 부모, ... 루트까지 올라가며 비교후 스왑
+     */
+    let currentIndex = this.store.length - 1;
+    let parentIndex = -1;
+
+    while (currentIndex !== 0) {
+      if (currentIndex % 2 === 1) {
+        parentIndex = (currentIndex - 1) / 2;
+      } else {
+        parentIndex = (currentIndex - 2) / 2;
+      }
+
+      if (!this.swap(currentIndex, parentIndex)) {
+        break;
+      }
+
+      currentIndex = parentIndex;
+    }
+  }
+
+  private heapifyToDownward(): void {
+    /**
+     * HACK: 왜 2번 도나? push와 pop이 반복되는 상황에서 최소힙인데 루트가 자식 둘보다 크거나 최대힙인데 루트가 자식 둘보다 작은 상황이 발생할 수 있음
+     * 예를 들어, 최소힙에서 0, 10, 100, -10, -100 순서대로 push를 진행하고 pop을 2번하면 [100, 10, 0]이 된다.
+     * 한 번만 루프가 돌면 [10, 100, 0]이 되어, 최소힙이 깨진다. 두 번 돌아야 한다.
      */
     for (let i = 0; i < 2; i++) {
       let currentIndex = 0;
@@ -63,33 +92,6 @@ export class Heap<T> {
 
         break;
       }
-    }
-  }
-
-  all(): T[] {
-    return this.store;
-  }
-
-  /**
-   *    1. 가장 후미에 아이템 삽입
-   *    2. 아이템의 부모, 부모의 부모, ... 루트까지 올라가며 비교후 스왑
-   */
-  private heapify(): void {
-    let currentIndex = this.store.length - 1;
-    let parentIndex = -1;
-
-    while (currentIndex !== 0) {
-      if (currentIndex % 2 === 1) {
-        parentIndex = (currentIndex - 1) / 2;
-      } else {
-        parentIndex = (currentIndex - 2) / 2;
-      }
-
-      if (!this.swap(currentIndex, parentIndex)) {
-        break;
-      }
-
-      currentIndex = parentIndex;
     }
   }
 
